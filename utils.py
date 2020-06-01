696da236c4fa02c5aff78fd9addb275b2eaf0a53
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-01 23:01:12 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -62,8 +62,8 @@ def trainfunction(task, train_loader):
 			loss = classLoss + distLoss
 			
 			# Get predictions
-			#cut_outputs = outputs[..., task : task + TASK_SIZE]
-			_, preds = torch.max(outputs.data, 1)
+			cut_outputs = outputs[..., task : task + TASK_SIZE]
+			_, preds = torch.max(cut_outputs.data, 1)
 			# Update Corrects
 			running_corrects += torch.sum(preds == labels.data).data.item()
 			loss.backward()  # backward pass: computes gradients
