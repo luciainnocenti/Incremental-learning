7fb260d67d350e4799f56086a0bb20bcf79728a1
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-02 18:04:02 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -85,7 +85,7 @@ def evaluationTest(task, test_loader, test_splits):
 		 v = np.array(x)
 		 col = np.concatenate( (col,v), axis = None)
 	col = col.astype(int)	
-
+	print(col)
 	for images, labels in test_loader:
 		images = images.float().to(params.DEVICE)
 		labels = labels.to(params.DEVICE)
