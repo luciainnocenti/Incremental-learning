44b1fc7c057453c60bd6a9bc662c146e03b12006
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-10 17:51:45 +0200

    Update ICaRLModel.py

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -89,8 +89,9 @@ class ICaRLStruct (nn.Module):
     current_step = 0
 
     for y in splits:
-      print('y = ' , y)
+      print('y = ' , int(y))
       print('len ex = ', len(self.exemplars))
+      y = int(y)
       if(self.exemplars[y] is not None):
         length = len(self.exemplars[y])
         exLabels = [y]*lenght #dovrebbe crearmi un vettore di dimensione lenght tutto composto da y ovvero la classe
