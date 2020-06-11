9f792b2aad84c0812b25f8bea60c2e0573524ddb
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-11 06:57:30 +0200

    Update ICaRLModel.py

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -41,7 +41,8 @@ class ICaRLStruct (nn.Module):
     images --> indexes of image from a class (Y) belonging to dataSet
     m --> num of elements to be selected for the class Y 
     '''
-    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+    #transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0,5), (0.5, 0.5, 0.5)),])
     features = []
 
     for idx in images:
