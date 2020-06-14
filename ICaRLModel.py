8657b3edfe20dec6d6e70677aabc7016a86b9a5a
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-14 16:12:26 +0200

    created new structure

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -13,187 +13,196 @@ from DatasetCIFAR.data_set import Dataset
 from torchvision import transforms
 from torch.utils.data import Subset, DataLoader
 
-class ICaRLStruct (nn.Module):
-  def __init__(self, n_classes = 100, dataset = None):
-    super(ICaRLStruct, self).__init__()
-    self.features_extractor = ResNet.resnet32(num_classes = 10) 
-    self.classifier = nn.Linear(self.features_extractor.fc.out_features, n_classes)
+#utils.py
+PARAMS = {
+	K = 2000
+	...
+}
 
-    self.k = 2000
-    self.exemplars = [None]*n_classes #lista di vettori; ha 100 elementi, ognuno di dimensione m che contiene gli m examplars
-    self.m = 0
-    self.dataset = dataset
-    self.exemplar_means = None
-    #Costruisco exemplar come un vettore di liste: 
-    #ogni elemento corrisponde alla lista di exemplar presente per quella specifica classe (l'indice di exemplar indica la classe)
-    #ogni lista avrà dimentsione M (variante di task in task dunque)
-    #Così per ottenere la lista di exemplar in analisi ogni volta posso usare col come con LWF
-    self.means = {}
-    self.cuda()
+def classify(images, exemplars, ICaRL, task):
 
-  def forward(self, x):
-    x = self.features_extractor(x)
-    x = self.classifier(x)
-    return x
+	preds = []
 
+	nClasses = task + params.TASK_SIZE
+	means = torch.zeros( ( nClasses, 64)).to(params.DEVICE)
 
-  def generateExemplars(self, images, m, idxY):
-    '''
-    images --> indexes of image from a class (Y) belonging to dataSet
-    m --> num of elements to be selected for the class Y 
-    '''
-    #transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
-    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5), (0.5, 0.5, 0.5)),])
-    features = []
+	ICaRL.train(False)
 
-    for idx in images:
-      self.cuda()
-      img = self.dataset._data[idx]
-      img = Variable(transform(Image.fromarray(img))).cuda()
-      feature = self.features_extractor(img.unsqueeze(0)).data.cpu().numpy()
-      '''
-      unsqueeze need to allow the network to read a single image. It create a "false" new dimension that simulates the following:
-      (n samples, channels, height, width).
-      Dunque anche il valore di feature sarà una matrice, ideata per contenere i valori di tutto il batch. 
-      Per questo motivo, nella seguente istruzione prendo solo il primo elemento ([0])
-      '''
-      features.append(feature[0]) 
-    features = np.array(features)
-    mu = np.mean(features, axis=0)
-    phiExemplaresY = []
-    exemplaresY = []
-    for k in range(0,m): #k parte da 0: ricorda di sommare 1 per media
-      phiX = features #le features di tutte le immagini della classe Y
-      phiP = np.sum(phiExemplaresY, axis = 0) #ad ogni step K, ho già collezionato K-1 examplars
-      mu1 = 1/(k+1)* ( phiX + phiP)
-      idxEx = np.argmin(np.sqrt(np.sum((mu - mu1) ** 2, axis=1))) #execute the euclidean norm among all the rows in phiX
-      
-      exemplaresY.append(images[idxEx])
-      np.delete(images, idxEx, 0)
-      phiExemplaresY.append(features[idxEx])
-      np.delete(features, idxEx, 0)
-    #Put into the exemplar array, at position related to the Y class, the elements obtained during this task
-    self.exemplars[idxY] = np.array(exemplaresY)
-    print('len exemplars[', idxY, '] = ', len(self.exemplars[idxY]))
-    for el in self.exemplars[idxY]:
-      if( self.dataset.__getitem__(el)[1] != idxY):
-        print("Problema!")
+	phiX = ICaRL(images, features = True)
+
+	transformer = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	ds = Dataset(train=True, transform = transformer)
+	classiAnalizzate = []
+
+	for i in range( 0, int(task/10) + 1) :
+		classiAnalizzate.append(ds.trainSplits[i])
+
+	for y in range (0, task + params.TASK_SIZE):
+		#now idxsImages contains the list of all the images selected as exemplars
+
+		classY = classiAnalizzate[y]
+
+		ss = Subset(ds, exemplars[classY])
+
+		loader = DataLoader( ss, num_workers=params.NUM_WORKERS, batch_size=params.BATCH_SIZE)
+	
+		for img, lbl, idx in loader:
+			with torch.no_grad:
+				x = ICaRL(img, features = True)
+			ma = torch.sum(x, dim=0)
+			means[y] += ma
+
+		means[y] = means[y]/ len(idx) # medio
 
 
-  def reduceExemplars(self, m):
-    for i in range(0, len(self.exemplars)):
-      if(self.exemplars[i] is not None):
-        self.exemplars[i] = np.array(self.exemplars[i])[:m]
+	for data in phiX:
+		pred = np.argmin(np.sqrt( np.sum((phiX - means)**2, axis=1)  ) )
+		preds.append(pred)
+
+	return torch.tensor(preds)
 
 
-  def updateRep(self, task, trainDataSet, splits, transformer):
-    #torch.cuda.empty_cache()
-    '''
-    trainDataSet is the subset obtained by extracting all the data having as label those contained into train splits
-    '''
-    D = Dataset(transform=transformer)
-    print(f'task = {task} ')
-    #Define the parameters for traininig:
-    optimizer = torch.optim.SGD(self.parameters(), lr=params.LR, momentum=params.MOMENTUM, weight_decay=params.WEIGHT_DECAY)
+
+
+
+
+
+def incrementalTrain(task, trainDS, ICaRL, exemplars):
+	trainSplits = trainDS.splits
+
+	train_indexes = trainDS.__getIndexesGroups__(task)
+
+
+  	col = []
+  	for i,x in enumerate( trainSplits[ :int(task/10) + 1]) : #comprende le 10 classi di questo task
+  	  v = np.array(x)
+  	  col = np.concatenate( (col,v), axis = None)
+  	  col = col.astype(int)
+  	print('col = ', col)
+
+
+  	train_ds = Dataset(train=True, transform = train_transformer)
+  	train_ds._data = trainDS._data[train_indexes]
+  	train_ds._targets = trainDS._targets[train_indexes]
+	
+	current_exemplars = exemplars[col[:10]]
+  	ICaRL = updateRep(task, train_ds, ICaRL, exemplars, trainSplits)
+
+  	m = params.K/(task + params.TASK_SIZE)
+
+  	exemplars = reduceExemplars(exemplars,m)
+
+  	exemplars = generateNewExemplars(exemplars, m, col[:10], trainDS, train_indexes, ICaRL)
+
+  	return ICaRL, exemplars
+
+def updateRep(task, train_indexes, ICaRL, exemplars, splits):
+
+	dataIdx = np.array(train_indexes)
+	for classe in exemplars:
+		if( classe is not None):
+			dataIdx = np.concatenate(dataIdx, classe)
+
+	#dataIdx contiene gli indici delle immagini, in train DS, delle nuove classi e dei vecchi exemplars
+
+	D = Subset(trainDS, dataIdx)
+
+	loader = DataLoader( D, num_workers=params.NUM_WORKERS, batch_size=params.BATCH_SIZE, shuffle = True)
+
+	old_ICaRL = deepcopy(ICaRL)
+
+	old_ICaRL.train(False)
+	ICaRL.train(True)
+
+
+	optimizer = torch.optim.SGD(ICaRL.parameters(), lr=params.LR, momentum=params.MOMENTUM, weight_decay=params.WEIGHT_DECAY)
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, params.STEP_SIZE, gamma=params.GAMMA) #allow to change the LR at predefined epochs
     current_step = 0
-    with torch.no_grad():
-      for y in splits:
-        y = int(y)
-        if(self.exemplars[y] is not None):
-          length = len(self.exemplars[y])
-          exLabels = [y]*length #dovrebbe crearmi un vettore di dimensione lenght tutto composto da y ovvero la classe
-          exImages = self.exemplars[y]
-          exImages = self.dataset._data[exImages]
-          D.append(exImages, exLabels)
 
-    loader = DataLoader( D, num_workers=params.NUM_WORKERS, batch_size=params.BATCH_SIZE, shuffle = True)
-    #Now D contains both images and examplers for classes in analysis
-    old_outputs = torch.zeros( len(D), 100).to(params.DEVICE)
-    #torch.cuda.empty_cache()
-    with torch.no_grad():
-      for img, lbl, idx in loader:
-        img = img.float().to(params.DEVICE)
-        idx = idx.to(params.DEVICE)
-        old_outputs[idx,:] = self.forward(img)
+    for i,x in enumerate( splits[ :int(task/10) + 1]):
+		v = np.array(x)
+		col = np.concatenate( (col,v), axis = None)
+	col = np.array(col).astype(int)
 
-    for p in self.features_extractor.parameters():
-            p.requires_grad = True
-    for p in self.classifier.parameters():
-            p.requires_grad = True
-    col = np.array(splits[int(task/10)]).astype(int)
-    for epoch in range(params.NUM_EPOCHS):
-      for images, labels, idx in loader:
-        images = images.float().to(params.DEVICE)
-        labels = labels.to(params.DEVICE)
+  	for epoch in range(params.NUM_EPOCHS):
+  	  lenght = 0
+  	  running_corrects = 0
+  	  
 
-        onehot_labels = torch.eye(100)[labels].to(params.DEVICE)
+  	  for images, labels, idx in loader:
+	
+  	    images = images.float().to(params.DEVICE)
+  	    labels = labels.to(params.DEVICE)
+  	    onehot_labels = torch.eye(100)[labels].to(params.DEVICE)
+  	    mappedLabels = utils.mapFunction(labels, col)
+	
+  	    optimizer.zero_grad()
+  	    
+  	    outputs = ICaRL(images, features = False)
+  	    old_outputs = old_ICaRL(images, features = False)
+	
+  	    loss = utils.calculateLoss(outputs, old_outputs, onehot_labels, task, splits )
+	
+  	    cut_outputs = np.take_along_axis(outputs.to(params.DEVICE), col[None,:], axis = 1).to(params.DEVICE)
+  	    _ , preds = torch.max(cut_outputs.data, 1)
+	
+  	    running_corrects += torch.sum(preds == mappedLabels.data).data.item()
+  	    lenght += len(images)
+	
+  	    loss.backward()  # backward pass: computes gradients
+  	    optimizer.step()
+  	  accuracy = running_corrects / float(lenght)
+  	  print("At step ", str(task), " and at epoch = ", epoch, " the loss is = ", loss.item(), " and accuracy is = ", accuracy)
+	
+	return ICaRL
 
-        optimizer.zero_grad()
+def reduceExemplars(exemplars,m):
+	exemplars = exemplars.copy
+	for i, el in enumerate(exemplars):
+	  if el is not None:
+		  exemplars[i] = el[:m,]
+	return exemplars	
 
-        outputs = self.forward(images)
-        loss = utils.calculateLoss(outputs, old_outputs[idx,:], onehot_labels, task, splits )
-        loss.backward()  # backward pass: computes gradients
-        optimizer.step()
-      print('Task: ' , task, ' epoch: ', epoch, ' loss: ', loss.item())
+def generateNewExemplars(exemplars, m, col, trainDS, train_indexes, ICaRL):
+	#col contiene i valori delle 10 classi in analisi in questo momento
 
+	for classe in col:
 
-  def classify(self, x, col, ds = None):
-    '''
-    x -> [BATCH SIZE] images to be classified
-    col -> list classes see until now
-    '''
-    if( ds is None):
-      ds = self.dataset
-    
-    with torch.no_grad() :
-      transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5), (0.5, 0.5, 0.5)),])
-      examplars = self.exemplars
-      phi = self.features_extractor
-      exemplar_means = []
-      for P_y in col: #itero per tutte le classi in analisi
-        P_y = int(P_y)
-        #print('Y = ', P_y)
-        features = []
-        #in P_y io ho m elementi, ovvero gli exemplars per quella specifica classe
-        if(self.exemplars[P_y] is not None):
-          #print('Not none P_y = ', P_y)
-          #exemplar contine gli indici delle immagini di riferiemnto
-          for ex in self.exemplars[P_y]:
-            image, label, idx = ds.__getitem__(ex)
-            
-            img = ds._data[ex]
-            img = Variable(transform(Image.fromarray(img))).cuda()
+		idxsImages = []
+    	for i in train_indexes:
+    		image, label, idx = trainDS.__getitem__(i)
+    		if( label == classe ):
+    			idxsImages.append(idx)
 
-            feature = phi(img.unsqueeze(0)) #unsqueeze add a dimension; i need it because feat ext expects a vector of imgs, not a single img
-            feature = feature.squeeze() # squeeze needed beacause phi return a matrix of features, on row for each img. But i have only 1 img
-            feature.data /= feature.data.norm()
-            features.append(feature)
-          #print('3:', len(features))
-          features = torch.stack(features)
-          #print('4:', features.size())
-          mu_y = features.mean(0).squeeze()
-          mu_y.data = mu_y.data / mu_y.data.norm() # Normalize
-          #print('5:', mu_y.size())
-          exemplar_means.append(mu_y)
-    self.exemplar_means = exemplar_means
-    #print('em = ', exemplar_means)
-    means = torch.stack(exemplar_means) 
-    #print('6: ', means.size())
-    means = torch.stack([means] * len(x)) #meglio usare len(x) che batch size per ultimo batch?
-    #print('7: ', means.size())
-    means = means.transpose(1, 2) 
-    #print('8: ', means.size())
-    with torch.no_grad():
-      feature = self.features_extractor(x) # (batch_size, feature_size)
-    for i in range(feature.size(0)): # Normalize
-      feature.data[i] = feature.data[i] / feature.data[i].norm()
-    feature = feature.unsqueeze(2) # (batch_size, feature_size, 1)
-    #print('9: ', feature.size())
-    feature = feature.expand_as(means) # (batch_size, feature_size, n_classes)
-    #print('10: ', feature.size())
-    
-    dists = (feature - means).pow(2).sum(1).squeeze() #(batch_size, n_classes)
-    #print('dists = ', dists)
-    _, preds = dists.min(1)
-    return preds
+    	exemplars[classe] = constructExemplars(idxsImages, m, ICaRL)
+
+def constructExemplars(idxsImages, m, ICaRL):
+	ICaRL = deepcopy(ICaRL).train(False)
+	transformer = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) ])
+
+	ds = Dataset(train = True, transform = transformer)
+
+	ss = Subset(ds, idxsImages)
+
+	loader = DataLoader( ss, num_workers=params.NUM_WORKERS, batch_size=params.BATCH_SIZE)
+	features = []
+	means = torch.zeros( (1, 64)).to(params.DEVICE)
+	for i, (image, label, idx) in enumerate(loader):
+		with torch.no_grad():
+			x = ICaRL( image, features = True)
+		for s in x:
+			features.append(np.array(s.data.cpu()))
+		ma = torch.sum(x, dim=0) #sommo sulle colonne, ovvero sulle features
+		means += ma
+	means = means/ len(idxsImages) # medio
+
+	newExs = []
+	phiNewEx = []
+	for k in range (0, m):
+		phiX = features #le features di tutte le immagini della classe Y
+      	phiP = np.sum(phiNewEx, axis = 0) #ad ogni step k, ho già collezionato k-1 examplars
+      	mu1 = 1/(k+1)* ( phiX + phiP)
+      	idxEx = np.argmin(np.sqrt(np.sum((mu - mu1) ** 2, axis=1))) #compute the euclidean norm among all the rows in phiX
+      	newExs.append(idxsImages[idxEx])
+      	phiExemplaresY.append(features[idxEx])
+    return newExs
