782b154048043644381b833401491345f0ed1b96
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-20 15:48:43 +0200

    Update ICaRLModel.py

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -57,7 +57,7 @@ def updateRep(task, trainDS, train_indexes, ICaRL, exemplars, splits):
 
 	D = Subset(trainDS, dataIdx)
 
-	loader = DataLoader( D, num_workers=params.NUM_WORKERS, batch_size=params.BATCH_SIZE, shuffle = False)
+	loader = DataLoader( D, num_workers=params.NUM_WORKERS, batch_size=params.BATCH_SIZE, shuffle = True)
 
 	old_ICaRL = deepcopy(ICaRL)
 
