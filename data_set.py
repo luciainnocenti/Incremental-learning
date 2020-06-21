959f6ae13e6822e942cc79eafee6ca60424fc0e0
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-21 18:41:04 +0200

    Update data_set.py

--- data_set.py
+++ data_set.py
@@ -67,3 +67,23 @@ class Dataset(torch.utils.data.Dataset):
  
   def __len__(self):
     return len(self._targets)
+
+class Subset(Dataset):
+    r"""
+    Subset of a dataset at specified indices.
+
+    Arguments:
+        dataset (Dataset): The whole Dataset
+        indices (sequence): Indices in the whole set selected for subset
+    """
+    def __init__(self, dataset, indices, transform):
+        self.dataset = dataset
+        self.indices = indices
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        im, labels = self.dataset[self.indices[idx]]
+        return self.transform(im), labels, idx
+
+    def __len__(self):
+        return len(self.indices)
