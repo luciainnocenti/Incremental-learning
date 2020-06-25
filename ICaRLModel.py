174b356cdb2684212aee364349914f0d7ea17734
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-25 20:36:02 +0200

    Update ICaRLModel.py

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -79,7 +79,7 @@ def updateRep(task, trainDS, train_indexes, ICaRL, exemplars, splits, transforme
 			images = images.float().to(params.DEVICE)
 			labels = labels.long().to(params.DEVICE)
 			
-			#onehot_labels = torch.eye(100)[labels].to(params.DEVICE)
+			onehot_labels = torch.eye(100)[labels].to(params.DEVICE)
 			
 			mappedLabels = utils.mapFunction(labels, col)
 			
@@ -90,7 +90,7 @@ def updateRep(task, trainDS, train_indexes, ICaRL, exemplars, splits, transforme
 			#weights = torch.sum( onehot_labels, dim=0)/torch.sum(onehot_labels) #prova con media fatta sul batch corrente
 			weights = utils.generateWeights(task, col).to(params.DEVICE)
 
-			loss = utils.calculateLoss(outputs, old_outputs, labels, task, splits, typeLoss = 'MSELoss', weights = weights)
+			loss = utils.calculateLoss(outputs, old_outputs, onehot_labels, task, splits, typeLoss = 'MSELoss', weights = weights)
 			
 			cut_outputs = np.take_along_axis(outputs.to(params.DEVICE), col[None,:], axis = 1).to(params.DEVICE)
 			_ , preds = torch.max(cut_outputs.data, 1)
