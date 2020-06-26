3206c55d652e68d7b1afb97b7f4123b5f542d307
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-26 08:33:10 +0200

    Update ICaRLModel.py

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -90,7 +90,8 @@ def updateRep(task, trainDS, train_indexes, ICaRL, exemplars, splits, transforme
 			#weights = torch.sum( onehot_labels, dim=0)/torch.sum(onehot_labels) #prova con media fatta sul batch corrente
 			weights = utils.generateWeights(task, col).to(params.DEVICE)
 			crt = nn.MSELoss()
-			loss = crt(outputs,onehot_labels)
+			m = nn.Softmax(dim=1)
+			loss = crt( m(outputs), onehot_labels)
 			#loss = utils.calculateLoss(outputs, old_outputs, onehot_labels, task, splits, typeLoss = 'MSELoss', weights = weights)
 			cut_outputs = np.take_along_axis(outputs.to(params.DEVICE), col[None,:], axis = 1).to(params.DEVICE)
 			_ , preds = torch.max(cut_outputs.data, 1)
