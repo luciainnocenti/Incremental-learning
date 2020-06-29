0bc4b2d7b13e315d11e468eda1dd61433c7670c4
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-29 21:57:09 +0200

    Update ICaRLModel.py

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -22,7 +22,7 @@ import random
 random.seed(params.SEED)
 
 def stage2(validationNewLoader, validationOldLoader, criterion, biasOptimizer, ICaRL, BIC, task, col):
-	m = nn.LogSoftmax()
+	#m = nn.LogSoftmax()
 	old_iterator = iter(validationOldLoader)
 	for imagesNew, labelsNew, _ in validationNewLoader:
 		
