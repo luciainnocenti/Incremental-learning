9ebbf7747c58c535bcb1959f71ed99af48a49c8a
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-30 11:49:50 +0200

    Update ICaRLModel.py

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -113,12 +113,12 @@ def updateRep(task, trainDS, train_indexes, ICaRL, exemplars, splits, transforme
 		valD = StdSubset(trainDS, validationOld)
 		validationOldLoader = DataLoader( valD, num_workers=params.NUM_WORKERS, batch_size=params.BATCH_SIZE)
 		criterion = nn.MSELoss()
-		biasOptimizer = torch.optim.SGD(BIC.parameters(), lr=params.BIAS_LR , weight_decay=params.BIAS_WEIGHT_DECAY )
-		biasScheduler = optim.lr_scheduler.MultiStepLR(biasOptimizer, params.BIAS_STEP_SIZE, gamma=params.BIAS_GAMMA)
+		biasOptimizer = torch.optim.SGD(BIC.parameters(), lr=0.0005 , weight_decay=params.BIAS_WEIGHT_DECAY )
+		#biasScheduler = optim.lr_scheduler.MultiStepLR(biasOptimizer, params.BIAS_STEP_SIZE, gamma=params.BIAS_GAMMA)
 		ICaRL.train(False)
 		for epoch in range(params.BIAS_NUM_EPOCHS ):
 			BIC = stage2(validationNewLoader, validationOldLoader, criterion, biasOptimizer, ICaRL, BIC, task, col)
-			biasScheduler.step()
+			#biasScheduler.step()
 			
 	old_ICaRL.train(False)
 	ICaRL.train(True)		
