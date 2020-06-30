728a47f248877ab4a6af3ce245c1f3df279f09da
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-06-30 16:30:10 +0200

    Update ICaRLModel.py

--- ICaRLModel.py
+++ ICaRLModel.py
@@ -192,6 +192,8 @@ def classify(images, exemplars, ICaRL, task, trainDS):
 			x /= torch.norm(x, p=2)
 		for s in x:
 			X_train.append(np.array(s.data.cpu()))
+			
+	for i, (image, label, idx) in enumerate(loader):
 		y_train = np.concatenate( (y_train, label) )
 				
 		
